66ac76ffe973d91e0384a743d7c14d36250c13d5
KayWong <93980326@qq.com>
2015-12-22 02:31:00 +0800

    Initial Commit

---
+++ VideoReverseDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoReverseDemo
+//
+//  Created by HuangKai on 15/12/22.
+//  Copyright © 2015年 HuangKai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoReverseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoReverseDemo
+//
+//  Created by HuangKai on 15/12/22.
+//  Copyright © 2015年 HuangKai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
